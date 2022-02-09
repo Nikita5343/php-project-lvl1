a8bcf1bce60430d9d8538e08585d2beeb3e7a060
Nikita Voitov <pochta5343@mail.ru>
2022-02-09 23:51:22 +0300

    it's good

---
+++ src/Cli.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Brain\Games\Cli;
+
+use function Cli\line;
+use function Cli\prompt;
+
+function welcome() {
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello %s!",$name);
+}
