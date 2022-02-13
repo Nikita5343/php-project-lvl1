3ab15d2c0fc920306c3b4d6b53b54446fdada0bf
Nikita Voitov <pochta5343@mail.ru>
2022-02-13 20:55:23 +0300

    add githubaction

--- src/Cli.php
+++ src/Cli.php
@@ -5,8 +5,9 @@ namespace Brain\Games\Cli;
 use function Cli\line;
 use function Cli\prompt;
 
-function welcome() {
+function welcome()
+{
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
-    line("Hello %s!",$name);
+    line("Hello %s!", $name);
 }
