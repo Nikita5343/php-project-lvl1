3f32cb4b800acf0cd901c4c926259718798510ec
Nikita Voitov <pochta5343@mail.ru>
2022-02-14 03:11:12 +0300

    lint norm

--- src/name.php
+++ src/name.php
@@ -5,7 +5,7 @@ namespace Brain\Games\Cli\Name;
 use function cli\line;
 use function cli\prompt;
 
-function welcome():string
+function welcome(): string
 {
     line('Добро пожаловать в интеллектуальную игру!');
     $name = prompt('Позвольте узнать ваше имя?');
