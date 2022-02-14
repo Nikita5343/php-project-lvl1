b644c47d9ede20e6b2380ebe5797f307c37895aa
Nikita Voitov <pochta5343@mail.ru>
2022-02-14 15:08:05 +0300

    d

---
+++ src/Games/brain-prime.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Brain\Prime;
+
+use function Brain\Games\welcome;
+use function Brain\general\engine;
+use function cli\line;
+use function cli\prompt;
+
+const COUNT = 3;
+
+function prostoe($num): bool
+{
+    if ($num < 2) {
+        return false;
+    }
+    for ($i = 2; $i <= $num / 2; $i++) {
+        if ($num % $i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function danet($num)
+{
+    $text = prostoe($num) ? 'да' : 'нет';
+    return $text;
+}
+
+function game(): void
+{
+    $name = welcome();
+    $result = '';
+    line("Ответьте «да», если данное число простое. В противном случае ответьте «нет».");
+    for ($i = 0; $i < COUNT; $i++) {
+        $num = rand(7, 11);
+        $question = prompt("$num");
+        $correctAnswer = danet($num);
+        $engine = engine($question, $correctAnswer);
+        if ($engine) {
+            $result = "Правильно! Поздравляем вас $name , Вы прошли игру)";
+        } else {
+            $result = "Давайте попробуем ещё раз, $name";
+            break;
+        }
+    }
+    line($result);
+}
