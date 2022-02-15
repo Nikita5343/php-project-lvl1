a50336fd06f32128062d991712b9d8594ce74c47
Nikita Voitov <pochta5343@mail.ru>
2022-02-15 16:56:08 +0300

    E6

--- Makefile
+++ Makefile
@@ -16,3 +16,5 @@ brain-progression:
 	./bin/brain-progression
 brain-prime:
 	./bin/brain-prime
+dump:
+	composer dump-autoload --optimize
--- src/Cli.php
+++ src/Cli.php
@@ -10,5 +10,4 @@ function welcome()
     line('Welcome to the Brain Games!');
     $name = prompt('May I have your name?');
     line("Hello %s!", $name);
-    return $name;
 }
