2030a943c9961e4ff3e3c5163af812250c881efc
Nikita Voitov <pochta5343@mail.ru>
2022-02-16 13:25:58 +0300

    ,

--- src/Cli.php
+++ src/Cli.php
@@ -9,6 +9,6 @@ function welcome()
 {
     line("Welcome to the Brain Games!");
     $name = prompt('May I have your name?');
-    line("Hello %s!", $name);
+    line("Hello, %s!", $name);
     return $name;
 }
