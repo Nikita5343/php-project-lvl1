ebf77a681d529897b5a4c7f9e426f0643ca7dad8
Nikita Voitov <pochta5343@mail.ru>
2022-02-20 23:56:40 +0300

    good linter-ok

--- src/Games/brain-gcd.php
+++ src/Games/brain-gcd.php
@@ -11,8 +11,8 @@ const COUNT = 3;
 
 function gcd($rand1, $rand2)
 {
-    for($i = $rand1, $j = 0; $j < $i; $i--) {
-        if($rand2 % $i === 0 && $rand1 % $i === 0) {
+    for ($i = $rand1, $j = 0; $j < $i; $i--) {
+        if ($rand2 % $i === 0 && $rand1 % $i === 0) {
             return $i;
         }
     }
