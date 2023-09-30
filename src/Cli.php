0241037f7df1c28a5b74d2726d02e6a9efc60c70
Никита Войтов <voitov@MacBook-Air-Nikita.local>
2023-09-30 22:10:45 +0300

    update level 3

---
+++ src/Cli.php
@@ -0,0 +1,18 @@
+<?php
+namespace BrainGames\Cli;
+$autoloadPath1 = __DIR__ . '/../../../autoload.php';
+$autoloadPath2 = __DIR__ . '/../vendor/autoload.php';
+if (file_exists($autoloadPath1)) {
+    require_once $autoloadPath1;
+} else {
+    require_once $autoloadPath2;
+}
+use function cli\line;
+use function cli\prompt;
+
+function hello(): void
+{
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello %s!", $name);
+}
